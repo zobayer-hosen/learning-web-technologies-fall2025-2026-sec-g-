88ee56224e3509817943333b279d9fc435f79f41
zobayer-hosen <zobayer.aiub.cse@gamil.com>
2025-11-17 17:01:05 +0600

    using suntract

--- Basic calculator/calculator.js
+++ Basic calculator/calculator.js
@@ -1,19 +1,63 @@
 const display = document.getElementById("display")
+let curValue = "";
+let operator = "";
+let previousValue = "";
 
 function appendToDisplay(input){
   display.value += input
 }
 
+// function clearDisplay(){
+//   display.value = "";
+// }
+// function calculate(){
+//   try{
+//     display.value = eval(display.value)
+//   }
+//   catch(error){
+//     display.value = "Error";
+
+//   }
+
+function Operator(op){
+  if (curValue == " "){
+    return;
+  }
+  if(previousValue != ""){
+    calculate();
+  }
+  operator= op;
+  previousValue = curValue;
+  curValue = "";
+}
 function clearDisplay(){
-  display.value = "";
+  curValue="";
+  previousValue= "";
+  operator= "";
+  display.value= "";
+}
+function add(num1, num2) {
+    return num1 + num2;
 }
 function calculate(){
-  try{
-    display.value = eval(display.value)
-  }
-  catch(error){
-    display.value = "Error";
+  if (previousValue === "" || curValue === "" || operator === "") {
+        return;
+    }
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        default:
+            return;
+    }
+    display.value = result;
+    curValue = result.toString();
+    previousValue = "";
+    operator = "";
 
   }
-}
+
+
+
+
 
